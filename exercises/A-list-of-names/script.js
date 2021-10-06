7bb78320cf5ac9d61f7e05e800179426f8e4b470
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-10-06 18:25:32 +0100

    Poeple and job added

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,16 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  arrayOfPeople.forEach(ppl => {
+    let heading1 = document.createElement("h1");
+    heading1.innerText = ppl.name;
+    content.appendChild(heading1);
+    let heading2 = document.createElement("h2");
+    heading2.innerText = ppl.job;
+    content.appendChild(heading2);
+  });
+  
+  
+
 }
 
 let people = [
