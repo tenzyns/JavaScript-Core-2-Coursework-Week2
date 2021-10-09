ae97072777b47646f0ff05d1e58729d1b6634629
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-10-09 19:24:05 +0100

    Shopping list items added to div

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,12 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(arrayOfItems) {
+  let content = document.querySelector("#content");
+  let unorderedLi = document.createElement("ul");
+  content.appendChild(unorderedLi);
+  arrayOfItems.forEach(item => {
+    let list = document.createElement("li");
+    list.innerText = item;
+    unorderedLi.appendChild(list);
+  })
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
