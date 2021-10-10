9009e8a15c93f708b72a05e54fe521a16bbefe3f
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-10-10 20:29:32 +0100

    dropdown list of colour  with select and option completed

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,33 @@
 function listOfColours(colours) {
-  // Write your code here...
+  const content = document.getElementById("content");
+  content.style.margin = 250;
+  const select = document.createElement("select");
+  
+  let para = document.createElement("p");
+  content.appendChild(select);
+  content.appendChild(para);
+  let option = document.createElement("option");
+  select.appendChild(option);
+    option.value = " "; //this bit creates the default selected value
+    option.innerText = "--Choose a colour--";
+
+  colours.forEach(colour => {
+    let option = document.createElement("option");
+    select.appendChild(option);
+    option.value = colour;
+    option.innerText = colour;
+    
+    
+    function changeColor(){
+      let selectedValue = select.options[select.selectedIndex].value;
+      //the above line was tricky to discover
+      para.style.color = selectedValue;
+      console.log("hello I am working");
+      para.innerText = `You have selected: ${selectedValue}`;
+    }
+    select.addEventListener("change", changeColor);
+  })
+  
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
