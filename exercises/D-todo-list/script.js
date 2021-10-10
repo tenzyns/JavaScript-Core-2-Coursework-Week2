beffced170623f320126928448b65c946f799819
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-10-10 16:46:39 +0100

    todo list completed

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,23 @@
 function todoList(todos) {
   // Write your code here...
+  const content = document.getElementById("content");
+  const unorderedList = document.createElement("ul");
+  content.appendChild(unorderedList);
+
+  todos.forEach(task => {
+    let taskList = document.createElement("li");
+    taskList.innerText = task.todo; //Each of the todo value text
+    unorderedList.appendChild(taskList);
+
+    function lineThrough(){
+      if (taskList.style.textDecoration !== "line-through"){
+      taskList.style.textDecoration = "line-through";
+    } else {
+      taskList.style.textDecoration = "none";
+    }
+    };   
+    taskList.addEventListener("click", lineThrough);
+  })
 }
 
 const todos = [
